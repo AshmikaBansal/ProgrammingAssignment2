3769d506b2773c44840f55acba2d88dc54e77af8
Ashmika Bansal <7.ashmika@gmail.com>
2020-06-27 13:52:09 +0530

    This is the assignment for the week 3 of R programming Course on Coursera :)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This takes input and will show you the matrix and its inverse.
+## First of all amking a cache matrix
+## Enter a square matrix only.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv  <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 
 }
 
 
-## Write a short comment describing this function
-
+## Now checking whether our cache matrix contains the inverse of matrix or not.
+## If not then it will calculate and set it to getinv function.
+## After this it returns the inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
